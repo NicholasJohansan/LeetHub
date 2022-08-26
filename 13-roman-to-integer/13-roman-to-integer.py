bece6050190edda4f4cd19a3725f22ffb5801377
NJ889 <58451243+NicholasJohansan@users.noreply.github.com>
2022-08-26 16:40:38 +0800

    Time: 119 ms (5.26%), Space: 14.1 MB (31.05%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,28 @@
+lazy_mapping = {
+    "IV": "IIII",
+    "IX": "VIIII",
+    "XL": "XXXX",
+    "XC": "LXXXX",
+    "CD": "CCCC",
+    "CM": "DCCCC"
+}
+
+numeral_mapping = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+}
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        for key, value in lazy_mapping.items():
+            s = s.replace(key, value)
+        num = 0
+        for char in s:
+            print(char)
+            num += numeral_mapping[char]
+        return num
